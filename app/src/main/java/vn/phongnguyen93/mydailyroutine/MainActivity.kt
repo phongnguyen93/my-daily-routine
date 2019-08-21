5879b6349d7de86c6363d3627a9150d812a0a4d5
Phong Nguyen <phongnguyen180993@gmail.com>
2019-08-21 22:00:52 +0700

    init commit from local

---
+++ app/src/main/java/vn/phongnguyen93/mydailyroutine/MainActivity.kt
@@ -0,0 +1,12 @@
+package vn.phongnguyen93.mydailyroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
